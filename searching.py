81d4dfd85f3f5faa5381d79219da7689b42dd7b7
Michal Mackovčin <280109@vutbr.cz>
2026-04-08 09:21:03 +0200

    ukol1

--- searching.py
+++ searching.py
@@ -27,5 +27,32 @@ def main():
     pass
 
 
+def read_data(file_name, pole):
+    # povolené klúče
+    povolene = ["unordered_numbers", "ordered_numbers", "dna_sequence"]
+
+    # kontrola klúča
+    if pole not in povolene:
+        return None
+
+    # cesta k súboru
+    file_path = Path.cwd() / file_name
+
+    # načítanie JSON suboru
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    # vrátenie dat podla klúču
+    return data[pole]
+
+def main():
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
+
+
 if __name__ == "__main__":
     main()
+
+def linear_search(file_name, pole):
+
+
